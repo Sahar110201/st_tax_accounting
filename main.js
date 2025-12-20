5a02da79bb719f25ade26f945451bc44be379914
Sahar110201 <saharadeeb7@gmail.com>
2025-12-20 02:10:50 -0500

    Initial commit

--- main.js
+++ main.js
@@ -4,16 +4,26 @@ const closeNavBtn = document.querySelector('#close__nav-btn');
 
 openNavBtn.addEventListener('click', () => {
   navItems.classList.add('open');
+  openNavBtn.classList.add('rotate');
 });
 
 closeNavBtn.addEventListener('click', () => {
   navItems.classList.remove('open');
+  openNavBtn.classList.remove('rotate');
+  closeNavBtn.classList.add('rotate');
+
+  setTimeout(() => {
+    closeNavBtn.classList.remove('rotate');
+  }, 400);
 });
 
-// Reset ONLY the menu when resizing to desktop
+// Reset nav if resized to >600px
 window.addEventListener('resize', () => {
-  if (window.innerWidth > 1024) {
+  if (window.innerWidth > 600) {
     navItems.classList.remove('open');
+    openNavBtn.classList.remove('rotate');
+    closeNavBtn.classList.remove('rotate');
   }
 });
+
 
